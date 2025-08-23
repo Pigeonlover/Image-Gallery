ce2f075064f0ef8f811bda2b74d7f92d6801b3e1
Pigeonlover <bravefoot@outlook.com>
2025-08-23 21:04:41 +0100

    Large image on first visit. Thumbnail container is centered. Thumbnail container moves to bottom of the screen when screen size < 500px.

--- app.js
+++ app.js
@@ -18,30 +18,59 @@ const images = [
     altText:
       "A black chequered pigeon drinking from a water fountain of a white stone dog statue.",
   },
+  {
+    url: "https://cdn.pixabay.com/photo/2018/05/11/15/55/city-pigeon-3390891_1280.jpg",
+    altText: "Two pigeons standing near each other on a fence bar.",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2020/10/28/16/55/pigeon-5694010_1280.jpg",
+    altText: "A black pigeon lying on the grass, resting.",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2025/06/20/13/52/pigeons-9670910_1280.jpg",
+    altText: "Two pigeon fighting on a cobbled pavement.",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2017/06/13/20/52/bird-2400116_1280.jpg",
+    altText: "A pigeon standing on the edge of a hole in an old stone wall.",
+  },
 ];
 
 //TODO: I need to create my thumbnail images
 
 function createThumbnails() {
   const thumbnailBox = document.querySelector("#thumbnail-box");
-  for (image of images) {
+  for (const image of images) {
     const imgTag = document.createElement("img");
     imgTag.src = image.url;
     imgTag.alt = image.altText;
     imgTag.className = "thumbnail-image";
-    imgTag.addEventListener("click", createLargeImageHandler);
+    imgTag.addEventListener("click", (e) => createLargeImageHandler(image));
     thumbnailBox.appendChild(imgTag);
   }
 }
 
-//TODO: I need to create my large images
+//TODO: large image of first picture in the arrays shows when first visiting the site
 
-function createLargeImageHandler() {
+function largeImageOnVisit() {
+  const largeImageBox = document.querySelector("#large-image-box");
+  const largeImg = document.createElement("img");
+  largeImg.src = images[0].url;
+  largeImg.alt = images[0].altText;
+  largeImg.className = "large-image";
+  largeImageBox.appendChild(largeImg);
+}
+
+largeImageOnVisit();
+
+//TODO: I need to create my large images when clicking the respective thumbnail
+
+function createLargeImageHandler(image) {
   const largeImageBox = document.querySelector("#large-image-box");
   largeImageBox.innerHTML = null;
   const largeImg = document.createElement("img");
   largeImg.src = image.url;
-  largeImg.alt = image.alt;
+  largeImg.alt = image.altText;
   largeImg.className = "large-image";
   largeImageBox.appendChild(largeImg);
 }
